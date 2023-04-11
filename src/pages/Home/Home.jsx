3d22a9b044f9c46e42998bd32b55a627a10236e8
Ahmed Samir <ahmedsamir96m@gmail.com>
2023-04-11 11:15:06 +0200

    fixing bugs

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TopHeader from "../../components/TopHeader/TopHeader";
+
+const Home = () => {
+  return (
+    <div>
+      <TopHeader />
+      Home
+    </div>
+  );
+};
+
+export default Home;
